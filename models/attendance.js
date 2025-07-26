f24a9ccfe61702b159d9299bcfcd4c12a0051067
Alejotier94 <alejotier@gmail.com>
2025-07-25 19:18:36 -0500

    se definieron las tablas correcion

--- models/attendance.js
+++ models/attendance.js
@@ -3,7 +3,7 @@ const sequelize = require ('../config/database')
 
 const Attendance = sequelize.define('Attendance', {
     id_user: {
-        type:DataTypes.INTEGER,
+        type:DataTypes.STRING,
         allowNull:false
     },
     date:{
