f71d6affdafdb04f5f0e3ded194c9a2c90251c5a
germanmarquez56 <germanmarquez17@gmai.com>
2025-07-25 19:26:35 -0500

    doctype 25/07

---
+++ models/doctype.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize =require('../config/database')
+
+const doctype = sequelize.define('doctype',{
+    type:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    }
+});
+
+
+
+
+
+
+
+
+module.exports = doctype;
