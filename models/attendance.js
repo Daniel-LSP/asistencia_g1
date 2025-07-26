0a6cdbb730d9bc1d749ebcd0f25380799a22ba9a
Alejotier94 <alejotier@gmail.com>
2025-07-25 19:13:04 -0500

    se definieron las tablas

---
+++ models/attendance.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require ('../config/database')
+
+const Attendance = sequelize.define('Attendance', {
+    id_user: {
+        type:DataTypes.INTEGER,
+        allowNull:false
+    },
+    date:{
+        type:DataTypes.DATEONLY,
+        allowNull:false,
+    },
+    entry_time:{
+        type:DataTypes.TIME,
+        allowNull:false
+    },
+    departure_time:{
+        type:DataTypes.TIME,
+        allowNull:false
+    },
+    id_course:{
+        type:DataTypes.INTEGER,
+        allowNull:false
+    },
+
+
+});
+
+
+module.exports = Attendance;
